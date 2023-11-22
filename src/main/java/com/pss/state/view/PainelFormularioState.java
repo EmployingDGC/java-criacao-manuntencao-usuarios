9d52eb05c10e0bcc38d19de1438b3288ef3486de
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-22 20:46:07 -0300

    fix: ajustando estado inicial do painel de formulario

--- src/main/java/com/pss/state/view/PainelFormularioState.java
+++ src/main/java/com/pss/state/view/PainelFormularioState.java
@@ -5,19 +5,34 @@ import com.pss.view.PainelFormulario;
 public class PainelFormularioState {
     private PainelFormulario painelFormulario;
 
-    public PainelFormularioState(PainelFormulario painelFormulario) {
-        this.painelFormulario = painelFormulario;
-    }
+    public void aplicarEstado() {
+        this.painelFormulario.getTela().setTitulo(null);
 
-    public void setEstado() {
         this.painelFormulario.getCheckBoxAdministrador().setVisible(false);
-
+        
+        this.painelFormulario.getScrollMensagem().setVisible(false);
+        this.painelFormulario.getLabelMensagem().setVisible(false);
+        this.painelFormulario.getCampoMensagem().setText(null);
+        
         this.painelFormulario.getCampoSenhaAtual().setVisible(false);
+        this.painelFormulario.getLabelSenhaAtual().setVisible(false);
+        this.painelFormulario.getCampoSenhaAtual().setText(null);
+        
         this.painelFormulario.getCampoNovaSenha().setVisible(false);
-        this.painelFormulario.getCampoMensagem().setVisible(false);
+        this.painelFormulario.getLabelNovaSenha().setVisible(false);
+        this.painelFormulario.getCampoNovaSenha().setText(null);
+        
         this.painelFormulario.getCampoUsuario().setVisible(false);
+        this.painelFormulario.getLabelUsuario().setVisible(false);
+        this.painelFormulario.getCampoUsuario().setText(null);
+        
         this.painelFormulario.getCampoSenha().setVisible(false);
+        this.painelFormulario.getLabelSenha().setVisible(false);
+        this.painelFormulario.getCampoSenha().setText(null);
+        
         this.painelFormulario.getCampoNome().setVisible(false);
+        this.painelFormulario.getLabelNome().setVisible(false);
+        this.painelFormulario.getCampoNome().setText(null);
         
         this.painelFormulario.getBotaoRegistrar().setVisible(false);
         this.painelFormulario.getBotaoEntrar().setVisible(false);
@@ -25,8 +40,12 @@ public class PainelFormularioState {
         this.painelFormulario.getBotaoSalvar().setVisible(false);
         this.painelFormulario.getBotaoVoltar().setVisible(false);
     }
-
+    
     public PainelFormulario getPainelFormulario() {
         return this.painelFormulario;
+    }
+
+    public void setPainelFormulario(PainelFormulario painelFormulario) {
+        this.painelFormulario = painelFormulario;
     }
 }
