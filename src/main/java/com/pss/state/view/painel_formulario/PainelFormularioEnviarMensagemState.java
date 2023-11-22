ada44d5d193124a3c73bbffc7e4a9f4f4451ea56
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-22 20:48:26 -0300

    feat: estado de evio de mensagem para o painel de formulário

---
+++ src/main/java/com/pss/state/view/painel_formulario/PainelFormularioEnviarMensagemState.java
@@ -0,0 +1,33 @@
+package com.pss.state.view.painel_formulario;
+
+import com.pss.state.view.PainelFormularioState;
+
+import com.pss.view.PainelFormulario;
+
+public class PainelFormularioEnviarMensagemState extends PainelFormularioState{
+    @Override
+    public void aplicarEstado() {
+        super.aplicarEstado();
+
+        PainelFormulario pf = this.getPainelFormulario();
+
+        pf.getTela().setTitulo("Enviar Mensagem");
+
+        pf.getScrollMensagem().setVisible(true);
+
+        pf.getCampoUsuario().setVisible(true);
+        pf.getCampoNome().setVisible(true);
+        
+        pf.getCampoUsuario().setEditable(false);
+        pf.getCampoNome().setEditable(false);
+
+        pf.getLabelMensagem().setVisible(true);
+        pf.getLabelUsuario().setVisible(true);
+        pf.getLabelNome().setVisible(true);
+
+        pf.getBotaoEnviar().setVisible(true);
+        pf.getBotaoVoltar().setVisible(true);
+
+        pf.revalidate();
+    }
+}
