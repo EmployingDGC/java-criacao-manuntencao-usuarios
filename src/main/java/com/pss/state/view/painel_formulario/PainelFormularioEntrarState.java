95177d2f558976f4140af4fb3fb0fe36baf18007
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-23 00:22:58 -0300

    feat: fazendo troca de ação do botão

--- src/main/java/com/pss/state/view/painel_formulario/PainelFormularioEntrarState.java
+++ src/main/java/com/pss/state/view/painel_formulario/PainelFormularioEntrarState.java
@@ -1,14 +1,24 @@
 package com.pss.state.view.painel_formulario;
 
+import java.awt.event.ActionListener;
+
 import com.pss.state.view.PainelFormularioState;
+
 import com.pss.view.PainelFormulario;
 
 public class PainelFormularioEntrarState extends PainelFormularioState {
+    private ActionListener acaoDoBotaoRegistrar;
+    private ActionListener acaoDoBotaoEntrar;
+
+    public PainelFormularioEntrarState(PainelFormulario painel) {
+        super(painel);
+    }
+
     @Override
     public void aplicarEstado() {
         super.aplicarEstado();
 
-        PainelFormulario pf = this.getPainelFormulario();
+        PainelFormulario pf = this.getPainel();
 
         pf.getTela().setTitulo("Entrar");
 
@@ -22,5 +32,23 @@ public class PainelFormularioEntrarState extends PainelFormularioState {
         pf.getBotaoEntrar().setVisible(true);
 
         pf.revalidate();
+    }
+    
+    public void setAcaoDoBotaoRegistrar(ActionListener acao) {
+        if (this.acaoDoBotaoRegistrar != null) {
+            this.getPainel().getBotaoRegistrar().removeActionListener(this.acaoDoBotaoRegistrar);
+        }
+
+        this.getPainel().getBotaoRegistrar().addActionListener(acao);
+        this.acaoDoBotaoRegistrar = acao;
+    }
+
+    public void setAcaoDoBotaoEntrar(ActionListener acao) {
+        if (this.acaoDoBotaoEntrar != null) {
+            this.getPainel().getBotaoRegistrar().removeActionListener(this.acaoDoBotaoEntrar);
+        }
+
+        this.getPainel().getBotaoEntrar().addActionListener(acao);
+        this.acaoDoBotaoEntrar = acao;
     }
 }
--- src/main/java/com/pss/state/view/painel_formulario/PainelFormularioRegistrarState.java
+++ src/main/java/com/pss/state/view/painel_formulario/PainelFormularioRegistrarState.java
@@ -1,15 +1,24 @@
 package com.pss.state.view.painel_formulario;
 
+import java.awt.event.ActionListener;
+
 import com.pss.state.view.PainelFormularioState;
 
 import com.pss.view.PainelFormulario;
 
 public class PainelFormularioRegistrarState extends PainelFormularioState {
+    private ActionListener acaoDoBotaoRegistrar;
+    private ActionListener acaoDoBotaoEntrar;
+
+    public PainelFormularioRegistrarState(PainelFormulario painel) {
+        super(painel);
+    }
+
     @Override
     public void aplicarEstado() {
         super.aplicarEstado();
 
-        PainelFormulario pf = this.getPainelFormulario();
+        PainelFormulario pf = this.getPainel();
 
         pf.getTela().setTitulo("Registrar");
 
@@ -25,5 +34,23 @@ public class PainelFormularioRegistrarState extends PainelFormularioState {
         pf.getBotaoEntrar().setVisible(true);
 
         pf.revalidate();
+    }
+    
+    public void setAcaoDoBotaoRegistrar(ActionListener acao) {
+        if (this.acaoDoBotaoRegistrar != null) {
+            this.getPainel().getBotaoRegistrar().removeActionListener(this.acaoDoBotaoRegistrar);
+        }
+
+        this.getPainel().getBotaoRegistrar().addActionListener(acao);
+        this.acaoDoBotaoRegistrar = acao;
+    }
+
+    public void setAcaoDoBotaoEntrar(ActionListener acao) {
+        if (this.acaoDoBotaoEntrar != null) {
+            this.getPainel().getBotaoRegistrar().removeActionListener(this.acaoDoBotaoEntrar);
+        }
+
+        this.getPainel().getBotaoEntrar().addActionListener(acao);
+        this.acaoDoBotaoEntrar = acao;
     }
 }
