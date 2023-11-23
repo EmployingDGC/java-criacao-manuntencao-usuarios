292902aec46ac0deb233b68a354e80b00c91f615
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-22 21:08:36 -0300

    fix: delegando a responsabilidade de decidir qual a tela principal para a presenter

--- src/main/java/com/pss/App.java
+++ src/main/java/com/pss/App.java
@@ -1,10 +1,9 @@
 package com.pss;
 
 import com.pss.presenter.TelaPrincipalPresenter;
-import com.pss.state.view.painel_formulario.PainelFormularioEntrarState;
 
 public final class App {
     public static void main(String[] args) {
-        new TelaPrincipalPresenter(new PainelFormularioEntrarState());
+        new TelaPrincipalPresenter();
     }
 }
--- src/main/java/com/pss/presenter/TelaPrincipalPresenter.java
+++ src/main/java/com/pss/presenter/TelaPrincipalPresenter.java
@@ -4,12 +4,15 @@ import com.pss.view.PainelFormulario;
 import com.pss.view.Tela;
 
 import com.pss.state.view.PainelFormularioState;
+import com.pss.state.view.painel_formulario.PainelFormularioEntrarState;
 
 public class TelaPrincipalPresenter {
     private Tela tela;
 
-    public TelaPrincipalPresenter(PainelFormularioState estadoInicial) {
+    public TelaPrincipalPresenter() {
         this.tela = new Tela();
+
+        PainelFormularioState estadoInicial = new PainelFormularioEntrarState();
         
         PainelFormulario pf = new PainelFormulario(this.tela, estadoInicial);
 
