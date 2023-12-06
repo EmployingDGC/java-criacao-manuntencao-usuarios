06a4cc9e51cf3b41cf4ead74a0d4bac4f2b5a7e5
Davi GC <daviguizanicarvalho@gmail.com>
2023-12-06 20:05:58 -0300

    feat: MensagemModel nas classes de usuários

--- src/main/java/com/pss/model/usuario/UsuarioModel.java
+++ src/main/java/com/pss/model/usuario/UsuarioModel.java
@@ -1,5 +1,9 @@
 package com.pss.model.usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pss.model.MensagemModel;
 import com.pss.model.PessoaModel;
 
 public class UsuarioModel extends PessoaModel {
@@ -7,10 +11,14 @@ public class UsuarioModel extends PessoaModel {
     private String senha;
     private String nome;
 
+    private List<MensagemModel> mensagensRecebidas;
+
     public UsuarioModel(String nome, String usuario, String senha) {
         super(nome);
         this.usuario = usuario;
         this.senha = senha;
+
+        this.mensagensRecebidas = new ArrayList<MensagemModel>();
     }
 
     public String getUsuario() {
@@ -31,5 +39,9 @@ public class UsuarioModel extends PessoaModel {
 
     public String getNome() {
         return this.nome;
+    }
+
+    public List<MensagemModel> getMensagensRecebidas() {
+        return this.mensagensRecebidas;
     }
 }
